2cf2c3d74e48925636f79accfac33bc676dac664
Erick Vallenas <evallenas@MacBook-Pro-de-Erick.local>
2024-04-10 05:33:08 -0500

    Initial Commit

--- app/Http/Controllers/PaymentGateways/DLocalGoController.php
+++ app/Http/Controllers/PaymentGateways/DLocalGoController.php
@@ -13,8 +13,41 @@ class DLocalGoController extends Controller
     public function store(Request $request)
     {
 
-        Log::info("Retornó");
-        Log::info($request);
+        $envApiKey = '1_DLGO_API_KEY';
+        $envSecretKey = '1_DLGO_SECRET_KEY';
+
+        $apiKey = env($envApiKey);
+        $secretKey = env($envSecretKey);
+
+        Log::info($request->payment_id);
+
+        $curl = curl_init();
+
+        curl_setopt_array(
+            $curl,
+            array(
+                CURLOPT_URL => "https://api-sbx.dlocalgo.com/v1/payments/" . $request->payment_id,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30000,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "GET",
+                CURLOPT_HTTPHEADER => array(
+                    "authorization: Bearer " . $apiKey . ":" . $secretKey,
+                    "content-type: application/json",
+                ),
+            )
+        );
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        Log::info($response);
+        Log::info($err);
+
+        curl_close($curl);
+
 
         // $paymentsController = new PaymentsController();
 
